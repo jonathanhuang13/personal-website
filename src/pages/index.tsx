6e4aaddbfc2e3ee610b4695371124ea134371ffa
Jonathan Huang <jonathan.huang1@gmail.com>
2021-05-16 21:02:42 -0500

    Change to under construction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,6 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
-interface LetterAttributes {
-  color: 'black' | 'green' | 'red' | 'yellow' | 'blue' | 'indigo' | 'pink';
-  decoration?: 'italic' | 'font-bold' | 'underline';
-}
-
-const letterAttributes: LetterAttributes[] = [
-  { color: 'green', decoration: 'font-bold' },
-  { color: 'black', decoration: 'italic' },
-  { color: 'red' },
-  { color: 'yellow' },
-  { color: 'blue', decoration: 'underline' },
-  { color: 'indigo' },
-  { color: 'pink', decoration: 'font-bold' },
-];
-
-const HAPPY = ['H', 'A', 'P', 'P', 'Y'];
-const BIRTHDAY = ['B', 'I', 'R', 'T', 'H', 'D', 'A', 'Y', '!', '!'];
-
 export const getServerSideProps: GetServerSideProps = async (_context) => {
   return { props: {} };
 };
@@ -27,24 +9,9 @@ export default function Home(): JSX.Element {
   return (
     <div className="flex flex-col justify-center h-screen">
       <Head>
-        <title>Elysa Kohrs</title>
+        <title>Jonathan Huang</title>
       </Head>
-      <div className="flex border-4 border-red-500 justify-center items-center self-center w-1/2 h-24">
-        {HAPPY.map((character) => (
-          <span className={`text-5xl mr-1 ${getRandomAttributes()}`}>{character}</span>
-        ))}
-        <span className="w-6" />
-        {BIRTHDAY.map((character) => (
-          <span className={`text-5xl mr-1 ${getRandomAttributes()}`}>{character}</span>
-        ))}
-      </div>
+      Under construction
     </div>
   );
-}
-
-function getRandomAttributes(): string {
-  const randomIndex = Math.floor(Math.random() * letterAttributes.length);
-  const attributes = letterAttributes[randomIndex];
-
-  return `text-${attributes.color}-500 ${attributes.decoration}`;
 }
