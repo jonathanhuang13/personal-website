cd4a0a0dcf9166db7149dbe39883c07eadc53f0b
Jonathan Huang <jonathan.huang1@gmail.com>
2021-08-08 23:00:45 -0700

    Add localhost to match

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
       beforeFiles: [
         {
           source: '/',
-          has: [{ type: 'host', value: '(?!www)(?<schoolSlug>.*).jonathanlhuang.com' }],
+          has: [{ type: 'host', value: '(?!www)(?<schoolSlug>.*).((jonathanlhuang.com)|(localhost.*))' }],
           destination: '/schoolSlug/:schoolSlug',
         },
       ],
