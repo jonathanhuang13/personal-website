f630952f5ad071504703827643173cb39ef21e7c
Jonathan Huang <jonathan.huang1@gmail.com>
2022-06-20 19:19:11 -0700

    Try counter.dev again

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,14 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document(): JSX.Element {
   return (
     <Html>
       <Head>
-        <Script
-          id="counter-dev"
+        <script
           dangerouslySetInnerHTML={{
-            __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"jonathanhuang",utcoffset:"-7"}))};sessionStorage.setItem("_swa","1");`,
+            __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"jonathanhuang",utcoffset:"-7"}))};sessionStorage.setItem("_swa","1");</script>`,
           }}
         />
       </Head>
