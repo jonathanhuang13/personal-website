957b28d7dbe5afd6bfc56c1c91c4d44fbe875b0b
Jonathan Huang <jonathan.huang1@gmail.com>
2022-06-20 18:50:28 -0700

    Try counter.dev

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document(): JSX.Element {
   return (
     <Html>
-      <Head />
+      <Head>
+        <Script id="counter.dev">
+          {`if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0)
+            {fetch(
+              'https://counter.dev/track?' +
+                new URLSearchParams({
+                  referrer: document.referrer,
+                  screen: screen.width + 'x' + screen.height,
+                  user: 'jonathanhuang',
+                  utcoffset: '-7',
+                }),
+            )}
+            ;sessionStorage.setItem("_swa","1");`}
+        </Script>
+      </Head>
       <body className="dark:bg-slate-900 text-black mx-4">
         <Main />
         <NextScript />
