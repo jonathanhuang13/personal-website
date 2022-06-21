17376387ff128a2433e449076172d57151801d49
Jonathan Huang <jonathan.huang1@gmail.com>
2022-06-20 19:32:12 -0700

    Try goat counter again

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,16 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document(): JSX.Element {
   return (
     <Html>
-      <Head></Head>
-      <body className="dark:bg-slate-900 text-black mx-4">
-        <Script
+      <Head>
+        <script
           data-goatcounter="https://jonathanhuang.goatcounter.com/count"
           async
           src="//gc.zgo.at/count.js"
-        />
+        ></script>
+      </Head>
+      <body className="dark:bg-slate-900 text-black mx-4">
         <Main />
         <NextScript />
       </body>
