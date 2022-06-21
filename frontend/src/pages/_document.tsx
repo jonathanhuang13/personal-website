888536cc4aaa276378ab9c2f3af8f1a4ae442290
Jonathan Huang <jonathan.huang1@gmail.com>
2022-06-20 19:37:34 -0700

    Don't count views locally

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -4,11 +4,17 @@ export default function Document(): JSX.Element {
   return (
     <Html>
       <Head>
+        {/* Only count on production */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `if (window.location.host !== 'jonathanlhuang.com') window.goatcounter = {no_onload: true}`,
+          }}
+        />
         <script
           data-goatcounter="https://jonathanhuang.goatcounter.com/count"
           async
           src="//gc.zgo.at/count.js"
-        ></script>
+        />
       </Head>
       <body className="dark:bg-slate-900 text-black mx-4">
         <Main />
