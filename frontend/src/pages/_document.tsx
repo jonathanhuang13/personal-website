9b1e111ab327db948da96f2791f34453271611ff
Jonathan Huang <jonathan.huang1@gmail.com>
2022-06-20 19:30:05 -0700

    Try goat counter

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,16 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document(): JSX.Element {
   return (
     <Html>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"jonathanhuang",utcoffset:"-7"}))};sessionStorage.setItem("_swa","1");</script>`,
-          }}
-        />
-      </Head>
+      <Head></Head>
       <body className="dark:bg-slate-900 text-black mx-4">
+        <Script
+          data-goatcounter="https://jonathanhuang.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+        />
         <Main />
         <NextScript />
       </body>
