34130c83db380bd55b7b4348ed6e0cb0d8bd2a2c
Jonathan Huang <jonathan.huang1@gmail.com>
2022-06-20 18:55:50 -0700

    Try counter.dev

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -5,19 +5,12 @@ export default function Document(): JSX.Element {
   return (
     <Html>
       <Head>
-        <Script id="counter.dev">
-          {`if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0)
-            {fetch(
-              'https://counter.dev/track?' +
-                new URLSearchParams({
-                  referrer: document.referrer,
-                  screen: screen.width + 'x' + screen.height,
-                  user: 'jonathanhuang',
-                  utcoffset: '-7',
-                }),
-            )}
-            ;sessionStorage.setItem("_swa","1");`}
-        </Script>
+        <Script
+          id="counter-dev"
+          dangerouslySetInnerHTML={{
+            __html: `<script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"jonathanhuang",utcoffset:"-7"}))};sessionStorage.setItem("_swa","1");</script>`,
+          }}
+        />
       </Head>
       <body className="dark:bg-slate-900 text-black mx-4">
         <Main />
