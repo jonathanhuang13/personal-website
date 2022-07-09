cb13258b22c7ea7ae0ffb84c5f7d3aa64b57c8c0
Jonathan Huang <jonathan.huang1@gmail.com>
2022-07-08 19:51:44 -0700

    Update subscribe copy

--- frontend/src/components/subscribe.tsx
+++ frontend/src/components/subscribe.tsx
@@ -11,17 +11,13 @@ export default function Subscribe({ className = '' }: Props): JSX.Element {
     <div
       className={`${className} mx-auto border dark:border-slate-600 bg-slate-100 dark:bg-slate-700 rounded-md p-4 flex flex-col items-center`}
     >
-      <span className="text-2xl font-bold mb-1">Want more content like this?</span>
-      <span className="text-sm mb-6 text-gray-500 dark:text-gray-400">
-        Subscribe to get my latest content by email. I won't send you spam and you can unsubscribe at any
+      <span className="text-2xl font-bold mb-1">Want to subscribe?</span>
+      <span className="text-sm mb-6 text-gray-500 dark:text-gray-400 text-center max-w-prose">
+        I don't write that much, but when I do, you'll be the first to know. You can always unsubscribe at any
         time.
       </span>
 
-      <ConvertKitForm
-        formId={CONVERTKIT_FORM_ID}
-        emailPlaceholder="Email"
-        namePlaceholder="First Name"
-      ></ConvertKitForm>
+      <ConvertKitForm formId={CONVERTKIT_FORM_ID} emailPlaceholder="Email" namePlaceholder="First Name" />
     </div>
   );
 }
