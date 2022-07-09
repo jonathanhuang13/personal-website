c3d930b432af357572f0b5d87ea459fa89ac46b6
Jonathan Huang <jonathan.huang1@gmail.com>
2022-07-08 20:06:57 -0700

    Fix mobile

--- frontend/src/components/convert-kit-form.tsx
+++ frontend/src/components/convert-kit-form.tsx
@@ -52,10 +52,10 @@ export default function ConvertKitForm({
   };
 
   return (
-    <form className={`${className} flex justify-center`} onSubmit={handleSubmit}>
+    <form className={`${className} sm:flex justify-center`} onSubmit={handleSubmit}>
       <input
         type="text"
-        className="py-1 px-2 border border-gray-300 rounded-md mr-2 dark:bg-slate-100 dark:text-gray-800"
+        className="py-1 px-2 border border-gray-300 rounded-md mb-2 sm:mb-0 sm:mr-2 dark:bg-slate-100 dark:text-gray-800"
         placeholder={namePlaceholder}
         aria-label={nameLabel}
         value={firstName}
@@ -63,14 +63,14 @@ export default function ConvertKitForm({
       />
       <input
         type="email"
-        className="py-1 px-2 border border-gray-300 rounded-md mr-2 dark:bg-slate-100 dark:text-gray-800"
+        className="py-1 px-2 border border-gray-300 rounded-md mb-2 sm:mb-0 mr-2 dark:bg-slate-100 dark:text-gray-800"
         placeholder={emailPlaceholder}
         aria-label={emailLabel}
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <button
-        className="py-1 px-6 rounded-md bg-slate-700 dark:bg-slate-900 text-white dark:text-gray-300 disabled:bg-slate-500 dark:disabled:bg-slate-600"
+        className="py-1 px-6 rounded-md bg-slate-700 dark:bg-slate-900 text-white dark:text-gray-300 disabled:bg-slate-500 dark:disabled:bg-slate-600 w-full sm:w-auto"
         disabled={isSubmitDisabled}
         type="submit"
       >
